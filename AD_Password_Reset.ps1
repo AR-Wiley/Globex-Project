534e98bd9bc40e0a9062da839d4d6d9810bc06d2
AR-Wiley <ar.wiley805@outlook.com>
2025-09-24 19:55:04 -0500

    first commit

---
+++ AD_Password_Reset.ps1
@@ -0,0 +1,23 @@
+﻿function Reset_Password {
+
+    param (
+        [string]$UID,
+        [string]$password
+    )
+
+    Try {
+
+        $securePassword = ConvertTo-SecureString -AsPlainText $password -Force 
+        
+        Set-ADAccountPassword -Identity $UID -NewPassword $securePassword
+
+        Get-ADUser -Identity $UID -ChangePasswordAtLogon $True
+
+        Write-Host "Password for user '$UID' has been reset successfully."
+    
+    } Catch {
+
+        Write-Host "An error has occurred: $($_.Exception.Message)"
+    }
+}
+
---
+++ Create-OU.ps1
@@ -0,0 +1,27 @@
+﻿function Create-OU {
+
+    param (
+        [string]$name
+    )
+
+    $OUPath = "DC=Globex,DC=com"
+
+    if (Get-ADOrganizationalUnit -Filter "Name -eq '$name'" -SeachBase $OUPath){
+    
+        Write-Host "Organizational Unit '$name' already exists" -ForegroundColor Yellow
+        return
+
+    }
+
+    Try{
+        
+        New-ADOrganizationalUnit -Name $name -Path "DC=Globex,DC=com" -ProtectedFromAccidentalDeletion $false 
+        Write-Host "Organizational Unit '$name' was successfully created." -ForegroundColor Green
+    
+    } Catch {
+    
+        Write-Host ("Failed to Create") -ForegroundColor Red 
+    
+    }
+
+}
